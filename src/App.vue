81d23811c3093d7ce5a3b2944c0b1dd8722dfb84
Aysu Nur Terzi <80470813+aysunurterzi@users.noreply.github.com>
2024-05-13 14:14:03 +0300

    first version memory game

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,97 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="startGame" v-if="!gameStarted">Oyuna Başla</button>
+    <div class="board" v-else>
+      <Card v-for="(card, index) in cards" :key="index" :data="card" @select="selectCard" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Card from './components/MemoryCard';
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    Card
+  },
+  data() {
+    return {
+      cards: [], 
+      selectedCards: [], 
+      gameStarted: false, 
+      timer: null, 
+      remainingTime: 60
+    };
+  },
+  methods: {
+    startGame() {
+      
+      this.gameStarted = true;
+      this.startTimer();
+      this.initializeCards();
+    },
+    startTimer() {
+      this.timer = setInterval(() => {
+        if (this.remainingTime > 0) {
+          this.remainingTime--;
+        } else {
+          clearInterval(this.timer);
+          alert("Oyun bitti!");
+        }
+      }, 1000);
+    },
+   initializeCards() {
+      const animals = ['aslan', 'fil', 'kedi', 'kopek', 'ordek', 'tavsan', 'timsah', 'zurafa'];
+      const shuffledAnimals = animals.concat(animals).sort(() => Math.random() - 0.5);
+      this.cards = shuffledAnimals.map((animal, index) => ({
+        id: index,
+        image: require(`./assets/img/${animal}.png`), // varsayılan olarak assets klasöründe olduğunu varsayalım
+        isOpen: false,
+        matched: false
+      }));
+    },
+
+selectCard(card) {
+  if (card.isOpen || this.selectedCards.length === 2 || card.matched) {
+    return;
   }
-}
+
+  this.selectedCards.push(card);
+  card.isOpen = true;
+
+  
+  if (this.selectedCards.length === 2) {
+    this.checkMatch();
+  }
+},
+
+
+    checkMatch() {
+      if (this.selectedCards[0].image === this.selectedCards[1].image) {
+        setTimeout(() => {
+          this.selectedCards.forEach(card => {
+            card.isOpen = false;
+          });
+          this.selectedCards = [];
+        }, 1000);
+      } else {
+        // Eşleşmeme durumu
+        setTimeout(() => {
+          this.selectedCards.forEach(card => {
+            card.isOpen = false;
+          });
+          this.selectedCards = [];
+        }, 1000);
+      }
+    }
+  }
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.board {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 10px;
 }
 </style>
---
+++ src/components/MemoryCard.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <div @click="handleClick" :class="{ 'card': true, 'open': isOpen }">
+    <img v-if="isOpen" :src="data.image" :alt="data.id">
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: Object // Kart verisi
+  },
+  data() {
+    return {
+      isOpen: false // Kartın açık olup olmadığını belirten bayrak
+    };
+  },
+  methods: {
+    handleClick() {
+      if (!this.isOpen) {
+        this.$emit('select', this);
+        this.isOpen = true;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.card {
+  width: 100px;
+  height: 100px;
+  background-color: #ccc;
+  display: inline-block;
+  margin: 5px;
+  cursor: pointer;
+}
+
+.open {
+  background-color: #fff;
+}
+</style>
